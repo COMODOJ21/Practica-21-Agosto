2251f63e049c42f4d40071b34b57e36af25d6b82
Nelson Gonzalez <andresecasvar05@gmail.com>
2024-08-21 13:33:10 -0500

    intento

---
+++ menu.py
@@ -0,0 +1,18 @@
+#crear un repositorio 
+#2. crear 3 ramas 
+
+#elabore un algoritmo que muestre un menu de opiones, las opciones son: 
+#a) jugar 
+#b) calcular 
+#c) salir 
+#este algoritmo va en alguna rama ya creada 
+
+# 2. elabore un algoritmo que calcule el promedio de las edades de esta ficha 
+# segunda rama
+
+print ("mostrar el menu")
+print("jugar")
+print("calcular")
+print("salir")
+
+    
